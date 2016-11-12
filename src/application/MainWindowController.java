c8fd26715944a865cec8153ce646031c12a7615f
Seil0 <jannik008@gmail.com>
2016-11-12 15:08:52 +0100

    updated to 0.3.9

--- src/application/MainWindowController.java
+++ src/application/MainWindowController.java
@@ -405,41 +405,6 @@ public class MainWindowController {
 	@FXML
 	private void debugBtnclicked(){
 		//for testing
-		
-		Alert alert = new Alert(AlertType.ERROR);
-    	alert.setTitle("Error");
-    	alert.setHeaderText("");
-    	alert.setContentText(errorUpdateV);
-    	
-    	Exception ex = new FileNotFoundException("Could not find file blabla.txt");
-
-    	// Create expandable Exception.
-    	StringWriter sw = new StringWriter();
-    	PrintWriter pw = new PrintWriter(sw);
-    	ex.printStackTrace(pw);
-    	String exceptionText = sw.toString();
-
-    	Label label = new Label("The exception stacktrace was:");
-
-    	TextArea textArea = new TextArea(exceptionText);
-    	textArea.setEditable(false);
-    	textArea.setWrapText(true);
-
-    	textArea.setMaxWidth(Double.MAX_VALUE);
-    	textArea.setMaxHeight(Double.MAX_VALUE);
-    	GridPane.setVgrow(textArea, Priority.ALWAYS);
-    	GridPane.setHgrow(textArea, Priority.ALWAYS);
-
-    	GridPane expContent = new GridPane();
-    	expContent.setMaxWidth(Double.MAX_VALUE);
-    	expContent.add(label, 0, 0);
-    	expContent.add(textArea, 0, 1);
-
-    	// Set expandable Exception into the dialog pane.
-    	alert.getDialogPane().setExpandableContent(expContent);
-
-    	alert.showAndWait();
-
 	}
 
 	
